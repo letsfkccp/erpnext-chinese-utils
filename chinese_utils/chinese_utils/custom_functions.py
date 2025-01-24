aa8f163090bb8a2d96f58b3150acfbc200d1aca6
letsfkccp <127059152+letsfkccp@users.noreply.github.com>
2025-01-24 22:01:40 +0000

    Initial commit: Add Chinese number conversion utility

---
+++ chinese_utils/chinese_utils/custom_functions.py
@@ -0,0 +1,44 @@
+import frappe
+
+def money_in_chinese(number):
+    chinese_nums = {
+        '0': '零',
+        '1': '壹',
+        '2': '贰',
+        '3': '叁',
+        '4': '肆',
+        '5': '伍',
+        '6': '陆',
+        '7': '柒',
+        '8': '捌',
+        '9': '玖'
+    }
+    
+    units = ['', '拾', '佰', '仟', '万']
+    
+    # Convert number to string with 2 decimal places
+    num_str = "{:.2f}".format(float(number))
+    integer_part, decimal_part = num_str.split('.')
+    
+    result = ''
+    
+    # Process integer part
+    for i, digit in enumerate(integer_part):
+        if digit != '0':
+            result += chinese_nums[digit] + units[len(integer_part) - i - 1]
+    
+    result += '圆'
+    
+    # Process decimal part
+    if decimal_part != '00':
+        jiao = decimal_part[0]
+        fen = decimal_part[1]
+        
+        if jiao != '0':
+            result += chinese_nums[jiao] + '角'
+        if fen != '0':
+            result += chinese_nums[fen] + '分'
+    else:
+        result += '整'
+    
+    return result
---
+++ chinese_utils/chinese_utils/hooks.py
@@ -0,0 +1,12 @@
+app_name = "chinese_utils"
+app_title = "Chinese Utils"
+app_publisher = "Your Name"
+app_description = "Chinese number conversion utilities for ERPNext"
+app_email = "your.email@example.com"
+app_license = "MIT"
+
+jenv = {
+    "methods": [
+        "chinese_utils.custom_functions.money_in_chinese"
+    ]
+}
